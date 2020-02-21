8b8795776073f944fe6412f31e868b9b4bbc6d41
Diego Rodríguez <mr.diego931@gmail.com>
2020-02-20 21:18:17 -0600

    Agregando crud usuarios

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 const express = require('express')
-//Cargamos la base de adtos
+    //Cargamos la base de adtos
 require('./db/mongoose')
-//Cargamos los enrutadores
-const User = require('./models/users')
-const MovRouter = require('./router/mov')
-//Usamos Express
+    //Cargamos los enrutadores
+    /* const User = require('./models/users')
+    const MovRouter = require('./router/mov') */
+const userRouter = require('./router/users')
+const movRouter = require('./router/mov')
+    //Usamos Express
 const app = express()
-//levanto el server
+    //levanto el server
 const port = process.env.PORT || 3001
 
 app.use(express.json())
+app.use(userRouter)
+app.use(movRouter)
 
 
-app.post('/users', (req, res) => {
-    const user = new User (req.body)
-
-    user.save().then(() => {
-        res.send(user)
-    }).catch((e) => {
-        res.send(e)
-    })
-})
 
 app.listen(port, () => {
     console.log('Servidor Levantado en el Puerto: ' + port);
---
+++ src/models/mov.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const Mov = mongoose.model('Mov', {
+
+})
+
+
+module.exports = router
